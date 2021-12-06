22bb143fd1b0d37a1f8dfa7e6df54c16a474e06d
Herkdes <utku.tenekecioglu@playtrick.co>
2021-12-06 22:33:06 +0300

    Initial Commit

---
+++ Assets/Scripts/Project/Runtime/Player/PlayerScoreframe.cs
@@ -0,0 +1,40 @@
+﻿namespace Base {
+    public class PlayerScoreframe : PlayerSubFrame {
+        #region Properties
+
+        public float PlayerScore;
+
+        #endregion Properties
+
+        #region Unity Functions
+
+        private void Start() {
+            SetupSubFrame();
+        }
+
+        #endregion Unity Functions
+
+        #region Spesific Functions
+
+        public void SetupScoreFrame() {
+            Parent.AddFramesToList(this);
+        }
+
+        public void OnPickupTaken(float value) { }
+
+        public override void SetupSubFrame() {
+            base.SetupSubFrame();
+            Parent.ScoreFrame = this;
+        }
+
+        #endregion Spesific Functions
+
+        #region Generic Functions
+
+        #endregion
+
+        #region IEnumerators
+
+        #endregion
+    }
+}
