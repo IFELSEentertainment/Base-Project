092ce8dd8290c16101da98f5d2840a1d602db27c
Dilaver Şerif <dilaver.serif@outlook.com>
2021-12-11 00:01:11 +0300

    Update PlayerAnimationController.cs

--- Assets/Runner Base/PlayerAnimationController.cs
+++ Assets/Runner Base/PlayerAnimationController.cs
@@ -9,8 +9,8 @@ using static MiniEnum;
 public class PlayerAnimationController : MonoBehaviour
 {
     private Animator anim;
-    public static Action<ChracterStat> ChangeAnim;
-    public ParticleSystem fireParticle;
+    
+    
     private void Awake()
     {
         anim = GetComponent<Animator>();
@@ -18,9 +18,7 @@ public class PlayerAnimationController : MonoBehaviour
     
     private void Start()
     {
-        GUIManager.GetButton(Enum_Menu_MainComponent.BTN_Start).AddFunction(()=> 
-            anim.SetBool("Start",true)
-        );
+
     }
 
 
@@ -48,15 +46,12 @@ public class PlayerAnimationController : MonoBehaviour
 
     private void OnScreenMouseDown(LeanFinger leanFinger)
     {
-        anim.SetBool("Fire",true);
-        //FireAnimTrigger(true);
+  
     }
     
     private void OnScreenMouseUp(LeanFinger leanFinger)
     {
-        anim.SetBool("Fire",false);
-        FireAnimTrigger(0);
-        //FireAnimTrigger(false,true);
+
     }
     
     private void OnScreenMouseDrag(LeanFinger leanFinger)
@@ -79,16 +74,4 @@ public class PlayerAnimationController : MonoBehaviour
         
     }
 
-    public void FireAnimTrigger(int a)
-    {
-        // if(!immediately) await Task.Delay(1250);
-        
-        if (a == 1)
-        {
-            if(Player.OnDown)
-                fireParticle.Play();
-        }
-        else fireParticle.Stop();
-    }
-    
 }
